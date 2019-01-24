e961e1533583e207e130f217fe9bad90e0a42f88
Shaundra <9601515+Shaundra@users.noreply.github.com>
2019-01-24 13:29:39 -0800

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,23 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].find { |person| person["status"] == "Winner" }["name"].split.first
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.values.flatten.find { |person| person["occupation"] == occupation }["name"]
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  data.values.flatten.map { |person| person["hometown"] == hometown ? 1 : 0 }
+                      .reduce(:+)
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.values.flatten.find { |person| person["hometown"] == hometown }["occupation"]
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages = data[season].map { |person| person["age"].to_f }
+  (ages.reduce(:+) / ages.size).round
 end
